a2a942e96229bd1a15c797cb51e50ca0e1bfbc97
Anna Zh <amie82@list.ru>
2023-04-25 11:01:38 +0700

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+function compare(a, b) {
+    if (a.key[0] < b.key[0]) { return -1; }
+    return 0;
+  }
+  
+  const sortingByProps = (object, arr) => {
+    const newArr = [];
+    const dirArr = [];
+  
+    for (const prop in object) {
+      if (arr.includes(prop)) {
+        newArr.push({ key: prop, value: object[prop] });
+      } else {
+        dirArr.push({ key: prop, value: object[prop] });
+      }
+    }
+    const result = newArr.concat(dirArr.sort(compare));
+  
+    return result;
+  };
+  
+  export default sortingByProps;
